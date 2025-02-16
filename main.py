fb734e231a97675dd6848c29459898be3b721f13
Darshan <darshanmadiwalar670@gmail.com>
2025-02-16 14:38:46 +0530

    updated

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from gmail import fetch_emails, creds
 from database import db
 from checker import read_rules, Processor
 
-limit = 5
+limit = 15
 emails = fetch_emails(limit)
 db.save_email_multiple(emails)
 emails = db.get_emails(limit)
 rule_processor = Processor("rules.json")
-matches = rule_processor.process_emails(emails, "rule-2")
+matches = rule_processor.process_emails(emails, "rule-3")
